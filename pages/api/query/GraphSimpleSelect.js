161437c01226f3af62082152df695dbccc7f6da7
Michele Lunghi <m.lunghi@metodafinance.it>
2024-01-21 15:05:49 +0100

    Fix(build): rimosse referenze a vecchio db

--- pages/api/query/GraphSimpleSelect.js
+++ pages/api/query/GraphSimpleSelect.js
@@ -1,28 +1,28 @@
-import { poolPromise, sql } from "../../../helpers/database";
-import FieldVisualizerModel from "../../../models/nosql_model/FieldVisualizerModel";
+// import { poolPromise, sql } from "../../../helpers/database";
+// import FieldVisualizerModel from "../../../models/nosql_model/FieldVisualizerModel";
 
-export default async (req, res) => {
-    try {
-        if (req.method !== "POST") {
-            return res.status(405).end(); // Metodo non consentito se non è POST
-        }
+// export default async (req, res) => {
+//     try {
+//         if (req.method !== "POST") {
+//             return res.status(405).end(); // Metodo non consentito se non è POST
+//         }
 
-        // Estrai dati dal body della richiesta
-        const { ConfigID, GraphID } = req.body;
+//         // Estrai dati dal body della richiesta
+//         const { ConfigID, GraphID } = req.body;
 
-        //ESTAGGO LA QUERY CON LE COORDINATE
+//         //ESTAGGO LA QUERY CON LE COORDINATE
 
-        const data = await FieldVisualizerModel.BaseGetOne(ConfigID);
+//         const data = await FieldVisualizerModel.BaseGetOne(ConfigID);
 
-        const bar = data.Bars[GraphID];
+//         const bar = data.Bars[GraphID];
 
-        const pool = await poolPromise;
+//         const pool = await poolPromise;
 
-        const result = await pool.request().query(bar.Query); //TODO GALATTICO: AGGIUNGERE TOKEN BEARER
+//         const result = await pool.request().query(bar.Query); //TODO GALATTICO: AGGIUNGERE TOKEN BEARER
 
-        if (result.recordset[0]) res.status(200).json(result.recordset[0]);
-        else res.status(500).send({ message: "No data" });
-    } catch (error) {
-        res.status(500).send({ message: "Error fetching data" });
-    }
-};
+//         if (result.recordset[0]) res.status(200).json(result.recordset[0]);
+//         else res.status(500).send({ message: "No data" });
+//     } catch (error) {
+//         res.status(500).send({ message: "Error fetching data" });
+//     }
+// };
--- pages/api/query/SimpleSelect.js
+++ pages/api/query/SimpleSelect.js
@@ -1,118 +1,118 @@
-import { poolPromise, sql } from "../../../helpers/database";
+// import { poolPromise, sql } from "../../../helpers/database";
 
-const getReq = async (req, res) => {
-    try {
-        const { QueryStructureContainer } = req.body;
+// const getReq = async (req, res) => {
+//     try {
+//         const { QueryStructureContainer } = req.body;
 
-        const resultContainer = [];
+//         const resultContainer = [];
 
-        await Promise.all(
-            QueryStructureContainer.map(async (QueryStructure) => {
-                //QueryStructure
-                //#region  select
-                let SelectQueryBlock = "SELECT ";
-                QueryStructure.select.map((el) => {
-                    SelectQueryBlock = SelectQueryBlock.concat(el + ",");
-                });
-                SelectQueryBlock = SelectQueryBlock.slice(0, -1); //rimuovo ultima virgola
-                //#endregion
+//         await Promise.all(
+//             QueryStructureContainer.map(async (QueryStructure) => {
+//                 //QueryStructure
+//                 //#region  select
+//                 let SelectQueryBlock = "SELECT ";
+//                 QueryStructure.select.map((el) => {
+//                     SelectQueryBlock = SelectQueryBlock.concat(el + ",");
+//                 });
+//                 SelectQueryBlock = SelectQueryBlock.slice(0, -1); //rimuovo ultima virgola
+//                 //#endregion
 
-                //#region from
-                const FromQueryBlock = " FROM " + QueryStructure.from;
-                //#endregion
-                //sezione where
-                //#region group
-                let GroupByQueryBlock = "";
-                if (QueryStructure.group && QueryStructure.group.length > 0) {
-                    GroupByQueryBlock = " Group By ";
-                    QueryStructure.group.map((el) => {
-                        GroupByQueryBlock = GroupByQueryBlock.concat(el + ",");
-                    });
-                    GroupByQueryBlock = GroupByQueryBlock.slice(0, -1); //rimuovo ultima virgola
-                }
+//                 //#region from
+//                 const FromQueryBlock = " FROM " + QueryStructure.from;
+//                 //#endregion
+//                 //sezione where
+//                 //#region group
+//                 let GroupByQueryBlock = "";
+//                 if (QueryStructure.group && QueryStructure.group.length > 0) {
+//                     GroupByQueryBlock = " Group By ";
+//                     QueryStructure.group.map((el) => {
+//                         GroupByQueryBlock = GroupByQueryBlock.concat(el + ",");
+//                     });
+//                     GroupByQueryBlock = GroupByQueryBlock.slice(0, -1); //rimuovo ultima virgola
+//                 }
 
-                //#region having
-                let HavingQueryBlock = "";
-                if (QueryStructure.having && QueryStructure.having.length > 0) {
-                    HavingQueryBlock = " Having ";
-                    QueryStructure.having.map((el) => {
-                        if (el.type == 0) {
-                            HavingQueryBlock = HavingQueryBlock.concat(el.column + " = " + el.value + " AND ");
-                        } else if (el.type == 1) {
-                            HavingQueryBlock = HavingQueryBlock.concat(el.column + " <> " + el.value + " AND ");
-                        } else if (el.type == 2) {
-                            HavingQueryBlock = HavingQueryBlock.concat(el.column + " > " + el.value + " AND ");
-                        } else if (el.type == 3) {
-                            HavingQueryBlock = HavingQueryBlock.concat(el.column + " < " + el.value + " AND ");
-                        } else if (el.type == 4) {
-                            HavingQueryBlock = HavingQueryBlock.concat(el.column + " >= " + el.value + " AND ");
-                        } else if (el.type == 5) {
-                            HavingQueryBlock = HavingQueryBlock.concat(el.column + " <= " + el.value + " AND ");
-                        } else if (el.type == 6) {
-                            HavingQueryBlock = HavingQueryBlock.concat(el.column + " BETWEEN " + el.value + " AND ");
-                        } else if (el.type == 7) {
-                            HavingQueryBlock = HavingQueryBlock.concat(el.column + " NOT BETWEEN " + el.value + " AND ");
-                        } else if (el.type == 8) {
-                            HavingQueryBlock = HavingQueryBlock.concat(el.column + " IN " + el.value + " AND ");
-                        } else if (el.type == 9) {
-                            HavingQueryBlock = HavingQueryBlock.concat(el.column + " NOT IN " + el.value + " AND ");
-                        } else if (el.type == 10) {
-                            HavingQueryBlock = HavingQueryBlock.concat(el.column + " LIKE %" + el.value + "% AND ");
-                        } else if (el.type == 11) {
-                            HavingQueryBlock = HavingQueryBlock.concat(el.column + " NOT LIKE %" + el.value + "% AND ");
-                        } else if (el.type == 12) {
-                            HavingQueryBlock = HavingQueryBlock.concat(el.column + " IS NULL " + el.value + " AND ");
-                        } else if (el.type == 13) {
-                            HavingQueryBlock = HavingQueryBlock.concat(el.column + " IS NOT NULL " + el.value + " AND ");
-                        }
-                    });
-                    HavingQueryBlock = HavingQueryBlock.slice(0, -4); //rimuovo ultima AND
-                }
+//                 //#region having
+//                 let HavingQueryBlock = "";
+//                 if (QueryStructure.having && QueryStructure.having.length > 0) {
+//                     HavingQueryBlock = " Having ";
+//                     QueryStructure.having.map((el) => {
+//                         if (el.type == 0) {
+//                             HavingQueryBlock = HavingQueryBlock.concat(el.column + " = " + el.value + " AND ");
+//                         } else if (el.type == 1) {
+//                             HavingQueryBlock = HavingQueryBlock.concat(el.column + " <> " + el.value + " AND ");
+//                         } else if (el.type == 2) {
+//                             HavingQueryBlock = HavingQueryBlock.concat(el.column + " > " + el.value + " AND ");
+//                         } else if (el.type == 3) {
+//                             HavingQueryBlock = HavingQueryBlock.concat(el.column + " < " + el.value + " AND ");
+//                         } else if (el.type == 4) {
+//                             HavingQueryBlock = HavingQueryBlock.concat(el.column + " >= " + el.value + " AND ");
+//                         } else if (el.type == 5) {
+//                             HavingQueryBlock = HavingQueryBlock.concat(el.column + " <= " + el.value + " AND ");
+//                         } else if (el.type == 6) {
+//                             HavingQueryBlock = HavingQueryBlock.concat(el.column + " BETWEEN " + el.value + " AND ");
+//                         } else if (el.type == 7) {
+//                             HavingQueryBlock = HavingQueryBlock.concat(el.column + " NOT BETWEEN " + el.value + " AND ");
+//                         } else if (el.type == 8) {
+//                             HavingQueryBlock = HavingQueryBlock.concat(el.column + " IN " + el.value + " AND ");
+//                         } else if (el.type == 9) {
+//                             HavingQueryBlock = HavingQueryBlock.concat(el.column + " NOT IN " + el.value + " AND ");
+//                         } else if (el.type == 10) {
+//                             HavingQueryBlock = HavingQueryBlock.concat(el.column + " LIKE %" + el.value + "% AND ");
+//                         } else if (el.type == 11) {
+//                             HavingQueryBlock = HavingQueryBlock.concat(el.column + " NOT LIKE %" + el.value + "% AND ");
+//                         } else if (el.type == 12) {
+//                             HavingQueryBlock = HavingQueryBlock.concat(el.column + " IS NULL " + el.value + " AND ");
+//                         } else if (el.type == 13) {
+//                             HavingQueryBlock = HavingQueryBlock.concat(el.column + " IS NOT NULL " + el.value + " AND ");
+//                         }
+//                     });
+//                     HavingQueryBlock = HavingQueryBlock.slice(0, -4); //rimuovo ultima AND
+//                 }
 
-                //#endregion
-                GroupByQueryBlock = GroupByQueryBlock + " " + HavingQueryBlock;
+//                 //#endregion
+//                 GroupByQueryBlock = GroupByQueryBlock + " " + HavingQueryBlock;
 
-                //#endregion
-                //sezione limit
-                //sezione join
+//                 //#endregion
+//                 //sezione limit
+//                 //sezione join
 
-                //#region order
-                let OrderByQueryBlock = "";
+//                 //#region order
+//                 let OrderByQueryBlock = "";
 
-                if (QueryStructure.order && QueryStructure.order.length > 0) {
-                    OrderByQueryBlock = " Order By ";
-                    QueryStructure.order.map((el) => {
-                        OrderByQueryBlock = OrderByQueryBlock.concat(el.column + " " + el.direction + ",");
-                    });
-                    OrderByQueryBlock = OrderByQueryBlock.slice(0, -1); //rimuovo ultima virgola
-                }
+//                 if (QueryStructure.order && QueryStructure.order.length > 0) {
+//                     OrderByQueryBlock = " Order By ";
+//                     QueryStructure.order.map((el) => {
+//                         OrderByQueryBlock = OrderByQueryBlock.concat(el.column + " " + el.direction + ",");
+//                     });
+//                     OrderByQueryBlock = OrderByQueryBlock.slice(0, -1); //rimuovo ultima virgola
+//                 }
 
-                //#endregion
-                const query = SelectQueryBlock + FromQueryBlock + GroupByQueryBlock + OrderByQueryBlock;
-                const pool = await poolPromise;
+//                 //#endregion
+//                 const query = SelectQueryBlock + FromQueryBlock + GroupByQueryBlock + OrderByQueryBlock;
+//                 const pool = await poolPromise;
 
-                const result = await pool.request().query(query);
+//                 const result = await pool.request().query(query);
 
-                resultContainer.push(result.recordset);
-            })
-        );
+//                 resultContainer.push(result.recordset);
+//             })
+//         );
 
-        console.log("resultContainer", resultContainer);
+//         console.log("resultContainer", resultContainer);
 
-        res.status(200).json(resultContainer);
-    } catch (error) {
-        res.status(500).send({ message: "Error fetching T_tabelle_query_editor data", error });
-    }
-};
+//         res.status(200).json(resultContainer);
+//     } catch (error) {
+//         res.status(500).send({ message: "Error fetching T_tabelle_query_editor data", error });
+//     }
+// };
 
-export default async (req, res) => {
-    try {
-        if (req.method === "POST") {
-            await getReq(req, res);
-        } else {
-            res.status(405).send({ message: "Method Not Allowed" });
-        }
-    } catch (error) {
-        res.status(500).send({ message: "Internal Server Error", error });
-    }
-};
+// export default async (req, res) => {
+//     try {
+//         if (req.method === "POST") {
+//             await getReq(req, res);
+//         } else {
+//             res.status(405).send({ message: "Method Not Allowed" });
+//         }
+//     } catch (error) {
+//         res.status(500).send({ message: "Internal Server Error", error });
+//     }
+// };
