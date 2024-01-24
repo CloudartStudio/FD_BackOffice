6c79915345cf532e0a34cb418a0c9669ec9a40d7
Michele Lunghi <m.lunghi@metodafinance.it>
2024-01-25 00:25:26 +0100

    FIX(CODEBASE): compilazione

--- pages/api/visualizer/chart/index.js
+++ pages/api/visualizer/chart/index.js
@@ -1,55 +1,55 @@
-import ChartTabModel from "../../../../models/nosql_model/ChartTabModel";
-import DynamicSections from "../../../../models/nosql_model/DynamicSections";
-import { getReq } from "../../dynamicSections/[id]";
+// import ChartTabModel from "../../../../models/nosql_model/ChartTabModel";
+// import DynamicSections from "../../../../models/nosql_model/DynamicSections";
+// import { getReq } from "../../dynamicSections/[id]";
 
-const postReq = async (req, res) => {
-    try {
-        const { GraphName, HexColor, SectionID, Bars } = req.body.newConfig;
+// const postReq = async (req, res) => {
+//     try {
+//         const { GraphName, HexColor, SectionID, Bars } = req.body.newConfig;
 
-        const dynPageRequest = new ChartTabModel(GraphName, HexColor, Bars);
+//         const dynPageRequest = new ChartTabModel(GraphName, HexColor, Bars);
 
-        const dynPage = await dynPageRequest.save();
+//         const dynPage = await dynPageRequest.save();
 
-        req.query.id = SectionID;
-        const SectionResult = await getReq(req, res);
+//         req.query.id = SectionID;
+//         const SectionResult = await getReq(req, res);
 
-        const newSection = new DynamicSections(SectionResult.NomeSezione, SectionResult.VerticalOrder, SectionResult.Tipo);
+//         const newSection = new DynamicSections(SectionResult.NomeSezione, SectionResult.VerticalOrder, SectionResult.Tipo);
 
-        newSection.CreationDate = Date.now();
-        newSection.IsActive = SectionResult.IsActive;
-        newSection.IsConfigured = true;
-        newSection.MinRole = SectionResult.MinRole;
-        newSection.RelatedConfigData = SectionResult.RelatedConfigData ? [...SectionResult.RelatedConfigData, dynPage] : [returnObj.config];
+//         newSection.CreationDate = Date.now();
+//         newSection.IsActive = SectionResult.IsActive;
+//         newSection.IsConfigured = true;
+//         newSection.MinRole = SectionResult.MinRole;
+//         newSection.RelatedConfigData = SectionResult.RelatedConfigData ? [...SectionResult.RelatedConfigData, dynPage] : [returnObj.config];
 
-        newSection.collectionName = "";
+//         newSection.collectionName = "";
 
-        const updatedSec = await newSection.UpdateWithColl(SectionID, "Sections");
+//         const updatedSec = await newSection.UpdateWithColl(SectionID, "Sections");
 
-        const returnObj = {
-            config: dynPage,
-            section: updatedSec,
-        };
+//         const returnObj = {
+//             config: dynPage,
+//             section: updatedSec,
+//         };
 
-        res.status(201).json(returnObj);
-    } catch (error) {
-        res.status(500).send({ message: "Error fetching data", error: error });
-    }
-};
+//         res.status(201).json(returnObj);
+//     } catch (error) {
+//         res.status(500).send({ message: "Error fetching data", error: error });
+//     }
+// };
 
-//DynamicPage
+// //DynamicPage
 
-export default async (req, res) => {
-    try {
-        if (req.method === "POST") {
-            postReq(req, res);
-            return;
-        } else {
-            res.status(500).send({
-                message: "Not Allowed",
-                error: error,
-            });
-        }
-    } catch (error) {
-        res.status(500).send({ message: "Error fetching data", error: error });
-    }
-};
+// export default async (req, res) => {
+//     try {
+//         if (req.method === "POST") {
+//             postReq(req, res);
+//             return;
+//         } else {
+//             res.status(500).send({
+//                 message: "Not Allowed",
+//                 error: error,
+//             });
+//         }
+//     } catch (error) {
+//         res.status(500).send({ message: "Error fetching data", error: error });
+//     }
+// };
